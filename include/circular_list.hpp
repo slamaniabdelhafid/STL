5bd729e5bbca2ef44d5283a0d75042e71ef220f7
slamaniabdelhafid <st130302@student.spbu.ru>
2025-06-01 23:36:40 +0300

    Update circular_list.hpp

--- include/circular_list.hpp
+++ include/circular_list.hpp
@@ -22,19 +22,57 @@ private:
     Node* head;
     size_t m_size;
 
+    void clean_up() noexcept {
+        if (!head) return;
+        
+        Node* current = head;
+        do {
+            Node* next = current->next;
+            delete current;
+            current = next;
+        } while (current != head);
+    }
+
 public:
-    // Iterator declarations
     class iterator;
     class const_iterator;
 
-    // Constructors/destructors
     circular_list() : head(nullptr), m_size(0) {}
     circular_list(std::initializer_list<T> init) : circular_list() {
-        for (const auto& item : init) push_back(item);
+        for (const auto& item : init) {
+            push_back(item);
+        }
     }
-    ~circular_list() { clear(); }
+    circular_list(const circular_list& other) : circular_list() {
+        for (const auto& item : other) {
+            push_back(item);
+        }
+    }
+    circular_list(circular_list&& other) noexcept 
+        : head(other.head), m_size(other.m_size) {
+        other.head = nullptr;
+        other.m_size = 0;
+    }
+    ~circular_list() { clean_up(); }
 
-    // Iterators
+    circular_list& operator=(const circular_list& other) {
+        if (this != &other) {
+            circular_list temp(other);
+            swap(temp);
+        }
+        return *this;
+    }
+    circular_list& operator=(circular_list&& other) noexcept {
+        if (this != &other) {
+            clean_up();
+            head = other.head;
+            m_size = other.m_size;
+            other.head = nullptr;
+            other.m_size = 0;
+        }
+        return *this;
+    }
+
     iterator begin() noexcept { return iterator(head); }
     const_iterator begin() const noexcept { return const_iterator(head); }
     const_iterator cbegin() const noexcept { return begin(); }
@@ -42,11 +80,9 @@ public:
     const_iterator end() const noexcept { return const_iterator(nullptr); }
     const_iterator cend() const noexcept { return end(); }
 
-    // Capacity
     bool empty() const noexcept { return m_size == 0; }
     size_t size() const noexcept { return m_size; }
 
-    // Element access
     T& front() {
         if (empty()) throw std::out_of_range("List is empty");
         return head->data;
@@ -64,19 +100,18 @@ public:
         return head->prev->data;
     }
 
-    // Modifiers
     void push_back(const T& value);
     void push_front(const T& value);
     void pop_back();
     void pop_front();
-    void clear() noexcept;
-
-    // Operations
+    void clear() noexcept { clean_up(); head = nullptr; m_size = 0; }
     void merge(circular_list& other);
     void reverse() noexcept;
-    void swap(circular_list& other) noexcept;
+    void swap(circular_list& other) noexcept {
+        std::swap(head, other.head);
+        std::swap(m_size, other.m_size);
+    }
 
-    // Iterator classes
     class iterator {
         Node* current;
     public:
